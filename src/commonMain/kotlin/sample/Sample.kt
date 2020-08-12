5485cec836ca792f3bbfe062aeb38fa0438ca06c
Atsushi KOSHIKIZAWA <lastarrow21@gmail.com>
2020-08-12 16:05:13 +0900

    modify to debug on IntelliJ IDEA

--- src/commonMain/kotlin/sample/Sample.kt
+++ src/commonMain/kotlin/sample/Sample.kt
@@ -2,6 +2,11 @@ package sample
 
 fun hello(): String = "Hello, Kotlin/Native!"
 
+fun sum(a: Int, b: Int): Int = a + b
+
 fun main() {
     println(hello())
+
+    val fortyTwo = sum(40, 2)
+    println(fortyTwo)
 }
